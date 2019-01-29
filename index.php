5f6da9ed82338b5b4d6f3105f6e97c61e7b405fb
Ondrej Vana <vana.ondrej@gmail.com>
2019-01-28 23:20:18 -0800

    feat: Rides list

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
 <?php
 
-phpinfo();
+declare(strict_types=1);
 
+use Kachnitel\RideTimeServer\Database\Connector;
+use Kachnitel\RideTimeServer\Database\Users;
+use Kachnitel\RideTimeServer\Database\Rides;
+
+// TODO: use guzzle?
+header('Content-Type: application/json');
+
+require_once(__DIR__ . '/vendor/autoload.php');
+
+$secretsFile = file_get_contents(__DIR__ . '/.secrets.json');
+$secrets = json_decode($secretsFile);
+
+$db = new Connector();
+$db->init($secrets->db);
+
+$q = filter_input(INPUT_GET, 'q', FILTER_SANITIZE_SPECIAL_CHARS);
+
+if ($q == 'rides') {
+    $rides = new Rides($db);
+
+    echo json_encode($rides->getRides());
+} else {
+    $users = new Users($db);
+
+    $userId = (int) filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+    echo json_encode($users->getUser($userId));
+}
---
+++ src/Database/Rides.php
@@ -0,0 +1,62 @@
+<?php
+namespace Kachnitel\RideTimeServer\Database;
+
+/**
+ * Rides
+ *
+ * @TODO:
+ *  Ride->members should be an array of base User
+ *  {id,name,profile_pic_url}
+ */
+class Rides
+{
+    public function __construct(Connector $connector)
+    {
+        /**
+         * @var Connector
+         */
+        $this->connector = $connector;
+    }
+
+    /**
+     * Return list of rides
+     *
+     * @return array
+     */
+    public function getRides(): array
+    {
+        $query = 'SELECT `event`.`id`,
+                          `event`.`created`,
+                          `event`.`start_time`,
+                          `event`.`name`,
+                          `event`.`description`,
+                          `event`.`route`,
+                          `event`.`difficulty`,
+                          `event`.`location` AS `location_id`,
+                          `event`.`duration`,
+                          `event`.`created_by`,
+                          `location`.`gps_lat`,
+                          `location`.`gps_lon`,
+                          `location`.`name` AS `location_name`
+                      FROM `ridetime`.`event`
+                              INNER JOIN `ridetime`.`location`
+                                      ON `location`.`id` = `event`.`location`;';
+
+        $rides = $this->connector->query($query);
+
+        $result = [];
+        foreach ($rides as $i => $ride) {
+            $result[] = (object) [
+                'name' => $ride['name'],
+                'difficulty' => (int) $ride['difficulty'],
+                'location' => $ride['location_name'],
+                'locationGps' => [$ride['gps_lat'], $ride['gps_lon']],
+                'members' => [1016, 1018], // TODO: separate query w/ details eventually
+                'terrain' => 'trail',
+                'route' => $ride['route']
+            ];
+        }
+
+        return $result;
+    }
+}
