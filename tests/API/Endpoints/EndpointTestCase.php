afca9a4ce486fcaf91afdda88bd7d5875afa9b07
Ondrej Vana <vana.ondrej@gmail.com>
2019-06-27 00:36:43 -0700

    test: Use configurable DB for entityManager in unit tests

--- tests/API/Endpoints/EndpointTestCase.php
+++ tests/API/Endpoints/EndpointTestCase.php
@@ -5,6 +5,7 @@ use PHPUnit\Framework\TestCase;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Setup;
 use Monolog\Logger;
+use function GuzzleHttp\json_decode;
 
 class EndpointTestCase extends TestCase
 {
@@ -13,29 +14,41 @@ class EndpointTestCase extends TestCase
      */
     protected $entityManager;
 
-    protected function setUp(): void {
+    protected function setUp(): void
+    {
         // Setup Doctrine
         $configuration = Setup::createAnnotationMetadataConfiguration(
             $paths = [__DIR__ . '/entities'],
             $isDevMode = true
         );
 
+        $secrets = $this->loadTestSecrets();
         // Setup connection parameters
         $connectionParameters = [
-            'dbname' => 'database',
-            'user' => 'user',
-            'password' => 'password',
-            'host' => 'host',
+            'dbname' => $secrets->db->database,
+            'user' => $secrets->db->user,
+            'password' => $secrets->db->password,
+            'host' => $secrets->db->host,
             'driver' => 'pdo_mysql'
         ];
 
         $this->entityManager = EntityManager::create($connectionParameters, $configuration);
     }
 
-    public function callMethod($obj, string $name, array $args) {
+    public function callMethod($obj, string $name, array $args)
+    {
         $class = new \ReflectionClass($obj);
         $method = $class->getMethod($name);
         $method->setAccessible(true);
         return $method->invokeArgs($obj, $args);
+    }
+
+    protected function loadTestSecrets(): object
+    {
+        $file = __DIR__ . '/../../../.secrets.test.json';
+        $contents = file_get_contents($file);
+        $decoded = json_decode($contents);
+
+        return $decoded;
     }
 }
