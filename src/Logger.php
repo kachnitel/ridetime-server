5e84fb9059e6732ad955c606f43d97df050ba881
Ondrej Vana <vana.ondrej@gmail.com>
2019-11-01 19:50:58 -0700

    feat: Configurable log level
    Set in config.json `log.level` key

--- src/Logger.php
+++ src/Logger.php
@@ -3,20 +3,25 @@ namespace RideTimeServer;
 
 use Monolog\Logger as Monolog;
 use Monolog\Formatter\JsonFormatter;
+use Monolog\Handler\StreamHandler;
 
 class Logger
 {
     /**
      * Initialize monolog
      *
-     * @param array $config ['appName', 'logPath']
+     * @param array $config ['appName', 'log': ['path', 'level']]
+     * - 'level' is one of Monolog\Logger constants (ie DEBUG = 100, INFO = 200, ...)
      * @return callable
      */
     public function getLogger(array $config): Monolog
     {
-        $logger = new \Monolog\Logger($config['appName']);
+        $logger = new Monolog($config['appName']);
 
-        $fileHandler = new \Monolog\Handler\StreamHandler($config['logPath']);
+        $fileHandler = new StreamHandler(
+            $config['log']['path'],
+            $config['log']['level'] ?? Monolog::DEBUG
+        );
         $fileHandler->setFormatter(new JsonFormatter());
         $logger->pushHandler($fileHandler);
         return $logger;
