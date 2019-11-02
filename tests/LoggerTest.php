7f87ba51632e4264ccb917cbf13393dfa6c7bf54
Ondrej Vana <vana.ondrej@gmail.com>
2019-11-01 23:56:35 -0700

    test: Fix LoggerTest using new config format

--- tests/LoggerTest.php
+++ tests/LoggerTest.php
@@ -4,13 +4,13 @@ namespace RideTimeServer\Tests;
 use PHPUnit\Framework\TestCase;
 use RideTimeServer\Logger;
 
-class UserTest extends TestCase
+class LoggerTest extends TestCase
 {
     public function testReturnsMonologInstance()
     {
         $logger = (new Logger())->getLogger([
             'appName' => 'test',
-            'logPath' => './test.log'
+            'log' => ['path' => './test.log']
         ]);
 
         $this->assertInstanceOf(\Monolog\Logger::class, $logger);
