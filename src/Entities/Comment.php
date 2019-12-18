b89a5af56f4834fcd1773374b43aeb5d4aae8d16
Ondrej Vana <vana.ondrej@gmail.com>
2019-12-17 19:35:41 -0800

    wip: Comment entity

---
+++ src/Entities/Comment.php
@@ -0,0 +1,199 @@
+<?php
+namespace RideTimeServer\Entities;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @Entity
+ * @Table(name="comment")
+ */
+class Comment
+{
+    /**
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var User
+     *
+     * @ManyToOne(targetEntity="User")
+     */
+    private $author;
+
+    /**
+     * @var \DateTime
+     *
+     * @Column(type="datetime")
+     */
+    private $timestamp;
+
+    /**
+     * @var Event
+     *
+     * @ManyToOne(targetEntity="Event")
+     */
+    private $event;
+
+    /**
+     * @var string
+     *
+     * @Column(type="string", length=2048)
+     */
+    private $message;
+
+    /**
+     * @var ArrayCollection|User[]
+     *
+     * @ManyToMany(targetEntity="User")
+     * @JoinTable(name="comment_seen_by",
+     *   joinColumns={@JoinColumn(name="user_id", referencedColumnName="id")},
+     *   inverseJoinColumns={@JoinColumn(name="comment_id", referencedColumnName="id", unique=true)}
+     * )
+     */
+    private $seenBy;
+
+    /**
+     * Get the value of id
+     *
+     * @return  int
+     */
+    public function getId(): int
+    {
+        return (int) $this->id;
+    }
+
+    /**
+     * Get the value of author
+     *
+     * @return User
+     */
+    public function getAuthor(): User
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set the value of author
+     *
+     * @param User $author
+     *
+     * @return self
+     */
+    public function setAuthor(User $author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of timestamp
+     *
+     * @return \DateTime
+     */
+    public function getTimestamp(): \DateTime
+    {
+        return $this->timestamp;
+    }
+
+    /**
+     * Set the value of timestamp
+     *
+     * @param \DateTime $timestamp
+     *
+     * @return self
+     */
+    public function setTimestamp(\DateTime $timestamp)
+    {
+        $this->timestamp = $timestamp;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of event
+     *
+     * @return Event
+     */
+    public function getEvent(): Event
+    {
+        return $this->event;
+    }
+
+    /**
+     * Set the value of event
+     *
+     * @param Event $event
+     *
+     * @return self
+     */
+    public function setEvent(Event $event)
+    {
+        $this->event = $event;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of message
+     *
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set the value of message
+     *
+     * @param string $message
+     *
+     * @return self
+     */
+    public function setMessage(string $message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of seenBy
+     *
+     * @return ArrayCollection|User[]
+     */
+    public function getSeenBy()
+    {
+        return $this->seenBy;
+    }
+
+    /**
+     * Set the value of seenBy
+     *
+     * @param ArrayCollection|User[] $seenBy
+     *
+     * @return self
+     */
+    public function setSeenBy($seenBy)
+    {
+        $this->seenBy = $seenBy;
+
+        return $this;
+    }
+
+    /**
+     * @param User $user
+     *
+     * @return self
+     */
+    public function addSeenBy(User $user)
+    {
+        $this->seenBy->add($user);
+
+        return $this;
+    }
+}
