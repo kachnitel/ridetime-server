9fa2d397de4486cf4852e5966acb8eb1d4ccc871
Ondrej Vana <vana.ondrej@gmail.com>
2020-01-06 17:01:09 -0800

    feat: Simplify AuthController->signIn
    Request no longer uses request body and searches by `authId`, eliminating possibility of a mismatch

--- src/API/Controllers/AuthController.php
+++ src/API/Controllers/AuthController.php
@@ -12,27 +12,6 @@ use function GuzzleHttp\json_decode;
 
 class AuthController extends BaseController
 {
-    /**
-     * Request data:
-     * {
-     *   "email": "ohooh@gmail.com",
-     *   "email_verified": true,
-     *   "family_name": "Na",
-     *   "gender": "male",
-     *   "given_name": "Kach",
-     *   "locale": "en-GB",
-     *   "name": "Kach Na",
-     *   "nickname": "hah",
-     *   "picture": "https://lh6.googleusercontent.com/-PsRsqMn9aHU/AAAAAAAAAAI/AAAAAAAAAAA/ACevoQNHk9xwM4StGhT42-2lCejGsz7AdA/mo/photo.jpg",
-     *   "sub": "google-oauth2|666",
-     *   "updated_at": "2019-02-14T06:24:15.673Z",
-     * }
-     *
-     * @param Request $request
-     * @param Response $response
-     * @param array $args
-     * @return Response
-     */
     public function signIn(Request $request, Response $response, array $args): Response
     {
         $token = $request->getAttribute('token');
@@ -44,29 +23,15 @@ class AuthController extends BaseController
         /** @var User $user */
         $user = $this->getUserRepository()->matching($criteria)->first();
 
-        if (!$user) {
-            return $response->withJson((object) [
+        $result = $user
+            ? (object) [
+                'success' => true,
+                'user' => $user->getDetail()
+            ]
+            : (object) [
                 'success' => false,
                 'errorCode' => 404
-            ]);
-        }
-
-        // Verify user from token
-        if ($authUserId !== $user->getAuthId()) {
-            $e = new UserException('Authentication ID mismatch', 400);
-            $e->setData([
-                'userId' => $user->getId(),
-                'expectedId' => $user->getAuthId(),
-                'requestTokenId' => $authUserId
-            ]);
-
-            throw $e;
-        }
-
-        $result = (object) [
-            'success' => true,
-            'user' => $user->getDetail()
-        ];
+            ];
 
         return $response->withJson($result);
     }
